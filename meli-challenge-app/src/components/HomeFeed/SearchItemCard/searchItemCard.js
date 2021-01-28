0dc6483507aa1a2f0c1657fc6885c642face7609
Joaquin Cayrus <jcayrus@gmail.com>
2021-01-27 21:18:18 -0300

    UPDATE: added components for search items

---
+++ meli-challenge-app/src/components/HomeFeed/SearchItemCard/searchItemCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './searchItemCard.scss';
+
+class SearchItemCard extends React.Component {
+    render() {
+        return (
+            <div className='card'>
+                CARD
+            </div>
+        );
+    }
+}
+
+export default SearchItemCard;
--- meli-challenge-app/src/components/HomeFeed/homeFeed.js
+++ meli-challenge-app/src/components/HomeFeed/homeFeed.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import './homeFeed.scss';
+import SearchItemCard from './SearchItemCard/searchItemCard';
+import ItemDetailCard from './ItemDetailCard/itemDetailCard';
 
 class HomeFeed extends React.Component {
     render() {
         return (
-            <p>
-                {/* TODO */}
-        Home feed
-            </p>
+            <div className='home-feed'>
+                {/* TODO Search Results */}
+                <div className='center container'>
+                    <SearchItemCard />
+                    <div className='horizontal-line center'></div>
+                    <SearchItemCard />
+                    <SearchItemCard />
+                    <SearchItemCard />
+                    <SearchItemCard />
+                </div>
+                {/* TODO Details */}
+                <div className='center container'>
+                    <ItemDetailCard />
+                </div>
+            </div>
         );
     }
 }
