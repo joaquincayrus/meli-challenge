481f16d7671328a7f7d28212a9bedce5c6c0b87e
Joaquin Cayrus <jcayrus@gmail.com>
2021-01-27 21:18:31 -0300

    UPDATE: added component for item detail

---
+++ meli-challenge-app/src/components/HomeFeed/ItemDetailCard/itemDetailCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './itemDetailCard.scss';
+
+class ItemDetailCard extends React.Component {
+    render() {
+        return (
+            <div className='card'>
+                DETAIL CARD
+            </div>
+        );
+    }
+}
+
+export default ItemDetailCard;
